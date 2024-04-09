ad6bc30510d4f8f86161ca53f76d0b4f52e7f873
EllmanT <tmuranda1@gmail.com>
2024-04-09 12:39:45 +0200

    Updater 75% done 2 parts now done

--- main.js
+++ main.js
@@ -1,26 +1,50 @@
-const fs = require('fs');
-const path = require('path');
-const os = require('os');
-const readline = require('readline');
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
+const readline = require("readline");
 
-const configFileName = 'config.ini';
-const certificateFolderName = 'certificate';
+const driverLetter = os.homedir().charAt(0).toUpperCase();
+const filePath = path.join(driverLetter + ":", "Revmax", "settings.ini");
+
+//part 1  Updating the settings .ini file//
+fs.readFile(filePath, "utf8", (err, data) => {
+  if (err) {
+    console.error("Error opening the file:", err);
+    return;
+  }
+  // Find and replace the text (case-insensitive)
+  const updatedData = data.replace(/zwl/gi, "ZiG");
+
+  // Write the updated contents back to the file
+  fs.writeFile(filePath, updatedData, "utf8", (err) => {
+    if (err) {
+      console.error("Error saving the file:", err);
+      console.error("Update failed!");
+      return;
+    }
+  });
+});
+
+//part 2 updating the swisbit config///
+
+const configFileName = "config.ini";
+const certificateFolderName = "certificate";
 
 // Get the list of available drives on the system
-const drives = os.platform() === 'win32' ? getWindowsDrives() : getUnixDrives();
+const drives = os.platform() === "win32" ? getWindowsDrives() : getUnixDrives();
 
 // Iterate through the drives to find the correct one
 let targetDrive = null;
-let configFilePath= null;
+let configFilePath = null;
 for (const drive of drives) {
   const driveRoot = path.join(drive, path.sep);
 
   // Skip the drive containing the operating system
-  if (os.platform() === 'win32' && driveRoot === os.platform() + '\\') {
+  if (os.platform() === "win32" && driveRoot === os.platform() + "\\") {
     console.log(`Skipping drive ${drive} (OS drive)`);
     continue;
   }
-  if (os.platform() !== 'win32' && driveRoot === '/') {
+  if (os.platform() !== "win32" && driveRoot === "/") {
     console.log(`Skipping drive ${drive} (OS drive)`);
     continue;
   }
@@ -28,7 +52,7 @@ for (const drive of drives) {
   console.log(`Checking drive ${drive}...`);
 
   const certificateFolderPath = path.join(driveRoot, certificateFolderName);
-   configFilePath = path.join(driveRoot, configFileName);
+  configFilePath = path.join(driveRoot, configFileName);
 
   console.log(`Checking certificate folder: ${certificateFolderPath}`);
   console.log(`Checking config file: ${configFilePath}`);
@@ -41,101 +65,127 @@ for (const drive of drives) {
 }
 
 if (!targetDrive) {
-  console.error(`Drive with '${certificateFolderName}' folder and '${configFileName}' file not found.`);
+  console.error(
+    `Drive with '${certificateFolderName}' folder and '${configFileName}' file not found.`
+  );
   return;
 }
 
 console.log(`Target drive found: ${targetDrive}`);
 
-fs.readFile(configFilePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error opening the file:', err);
-      return;
-    }
-  
-    console.log("File opened");
-  
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-  
-    let vatNumber = null;
-    let previousReceiptDateExists = false;
-    let isVATvalid = false;
-    let count = 10;
-  
-    rl.on('line', (input) => {
-      const trimmedInput = input.trim();
-      if (trimmedInput.length === 9) {
-        vatNumber = trimmedInput;
-        isVATvalid = true;
-        console.log("VAT is Valid");
-        rl.close();
-      } else {
-        count--;
-        console.log(`VAT is not valid (must be 9 characters). ${count} attempts left.`);
-        if (count > 0) {
-          rl.prompt();
-        } else {
-          console.log("No attempts left. Exiting.");
-          rl.close();
-        }
-      }
-    });
-  
-    console.log("Enter your VAT Number (9 characters):");
-    rl.prompt();
-  
-    rl.on('close', () => {
-      if (isVATvalid) {
-        const lines = data.split('\n');
-        let updatedData = '';
-        let vatNumberUpdated = false;
-  
-        for (let i = 0; i < lines.length; i++) {
-          const line = lines[i];
-  
-          if (line.match(/^PreviousReceiptDate:/i)) {
-            previousReceiptDateExists = true;
-          }
-  
-          if (line.match(/^VATNumber:/i)) {
-            updatedData += `VATNumber: ${vatNumber}\n`;
-            vatNumberUpdated = true;
-          } else {
-            updatedData += line + '\n';
-          }
-        }
-  
-        if (!vatNumberUpdated) {
-          updatedData += `VATNumber: ${vatNumber}\n`;
-        }
-  
-        if (!previousReceiptDateExists) {
-            const currentDate = new Date().toISOString();
-            updatedData += `PreviousReceiptDate: ${currentDate}\n`;
-          }
-        fs.writeFile(configFilePath, updatedData, 'utf8', (err) => {
-          if (err) {
-            console.error("Error saving the file:", err);
-            console.error("Update failed!");
-          } else {
-            console.log("File saved successfully.");
-            console.log("Update successful!");
-          }
-        });
-      }
-    });
+fs.readFile(configFilePath, "utf8", (err, data) => {
+  if (err) {
+    console.error("Error opening the file:", err);
+    return;
+  }
+
+  console.log("File opened");
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
+
+  let vatNumber = null;
+  let previousReceiptDateExists = false;
+  let isVATvalid = false;
+  let count = 10;
+
+  rl.on("line", (input) => {
+    const trimmedInput = input.trim();
+    if (trimmedInput.length === 9) {
+      vatNumber = trimmedInput;
+      isVATvalid = true;
+      console.log("VAT is Valid");
+      rl.close();
+    } else {
+      count--;
+      console.log(
+        `VAT is not valid (must be 9 characters). ${count} attempts left.`
+      );
+      if (count > 0) {
+        rl.prompt();
+      } else {
+        console.log("No attempts left. Exiting.");
+        rl.close();
+      }
+    }
+  });
+
+  console.log("Enter your VAT Number (9 characters):");
+  rl.prompt();
+
+  rl.on("close", () => {
+    if (isVATvalid) {
+      const lines = data.split("\n");
+      let updatedData = "";
+      let vatNumberUpdated = false;
+
+      for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+
+        if (line.match(/^PreviousReceiptDate:/i)) {
+          previousReceiptDateExists = true;
+        }
+
+        if (line.match(/^VATNumber:/i)) {
+          updatedData += `VATNumber: ${vatNumber}\n`;
+          vatNumberUpdated = true;
+        } else {
+          updatedData += line + "\n";
+        }
+      }
+
+      if (!vatNumberUpdated) {
+        updatedData += `VATNumber: ${vatNumber}\n`;
+      }
+
+      if (!previousReceiptDateExists) {
+        const currentDate = new Date().toISOString();
+        updatedData += `PreviousReceiptDate: ${currentDate}\n`;
+      }
+      fs.writeFile(configFilePath, updatedData, "utf8", (err) => {
+        if (err) {
+          console.error("Error saving the file:", err);
+          console.error("Update failed!");
+        } else {
+          console.log("Update Settings successful!");
+          console.log("Update Config successful");
+          console.log("Files SAVED successfully.");
+
+          process.stdin.setRawMode(true);
+          process.stdin.resume();
+          process.stdin.setEncoding("utf8");
+          console.log("");
+          console.log("UPGRADE DONE!!");
+          console.log("Press 'y' key to close the program.");
+
+          process.stdin.on("keypress", (key) => {
+            if (key === "y") {
+              console.log("Closing the program...");
+              console.log("you can now fiscalise in ZiG ;)")
+              process.stdin.pause();
+              process.stdin.removeAllListeners("keypress");
+              process.stdin.setRawMode(false);
+              process.exit();
+            }
+          });
+        }
+      });
+    }
+  });
+});
+
 // Continue with the rest of the code for the target drive
+
+//functions
 
 // Function to get the list of Windows drives
 function getWindowsDrives() {
   const drives = [];
   for (let i = 65; i <= 90; i++) {
     const driveLetter = String.fromCharCode(i);
-    const drivePath = driveLetter + ':\\';
+    const drivePath = driveLetter + ":\\";
     if (fs.existsSync(drivePath)) {
       drives.push(drivePath);
     }
@@ -145,5 +195,5 @@ function getWindowsDrives() {
 
 // Function to get the list of Unix drives
 function getUnixDrives() {
-  return fs.readdirSync('/').map((entry) => path.join('/', entry));
+  return fs.readdirSync("/").map((entry) => path.join("/", entry));
 }
